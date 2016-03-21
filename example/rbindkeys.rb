60c9b6ca68d85a950f58e29f379cd20045302306
Billy.Zheng <zw963@163.com>
2016-03-21 11:50:51 +0800

    update example
    Former-commit-id: 69779cbdffae4ef265f82a6ba5ac7097bedf7666

--- example/rbindkeys.rb
+++ example/rbindkeys.rb
@@ -12,36 +12,42 @@
 pre_bind_key KEY_CAPSLOCK, KEY_LEFTCTRL
 pre_bind_key KEY_RIGHTALT, KEY_CAPSLOCK
 
-# bind Ctrl+J => left
-bind_key [KEY_LEFTCTRL, KEY_J], KEY_LEFT
-# bind Ctrl+L => right
-bind_key [KEY_LEFTCTRL, KEY_L], KEY_RIGHT
+# bind Ctrl+B => left
+bind_key [KEY_LEFTCTRL, KEY_B], KEY_LEFT
+# bind Ctrl+F => right
+bind_key [KEY_LEFTCTRL, KEY_F], KEY_RIGHT
 
-# bind Alt+J => Ctrl+left
-bind_key [KEY_LEFTALT, KEY_J], [KEY_LEFTCTRL, KEY_LEFT]
-# bind Alt+L => Ctrl+right
-bind_key [KEY_LEFTALT, KEY_L], [KEY_LEFTCTRL, KEY_RIGHT]
+# bind Alt+B => Ctrl+left
+bind_key [KEY_LEFTALT, KEY_B], [KEY_LEFTCTRL, KEY_LEFT]
+# bind Alt+F => Ctrl+right
+bind_key [KEY_LEFTALT, KEY_F], [KEY_LEFTCTRL, KEY_RIGHT]
+
+# paste
+bind_key [KEY_LEFTCTRL, KEY_Y], [KEY_LEFTSHIFT, KEY_INSERT]
 
 #
 # ============================== global default binding end ==============================
 #
 
-# For browser, will overwrite glboal default debinding
+# For browser, will overwrite glboal default keybinding.
 window(@default_bind_resolver, :app_class => /Firefox$|^chromium-browser$|^google-chrome$/) do
-  # search bar
-  bind_key [KEY_LEFTCTRL, KEY_S], [KEY_LEFTCTRL, KEY_K]
+  bind_key [KEY_LEFTCTRL, KEY_S], [KEY_LEFTCTRL, KEY_F]
 end
 
 def terminal_global
+  # enlarge font
   bind_key [KEY_LEFTCTRL, KEY_EQUAL], [KEY_LEFTSHIFT, KEY_LEFTCTRL, KEY_EQUAL]
 
-  # bind Ctrl+. to output a ` => '.
+  # bind Ctrl+. to output a ` => ', Here use block form.
   bind_key [KEY_LEFTCTRL, KEY_DOT] do |_ev, op|
+    # release recent pressed modifier key first. (maybe it is a bug.)
     op.release_key KEY_LEFTCTRL
 
+    # press space key
     op.press_key KEY_SPACE
     op.release_key KEY_SPACE
 
+    # press equal key
     op.press_key KEY_EQUAL
     op.release_key KEY_EQUAL
 
@@ -49,12 +55,13 @@ def terminal_global
     # here press Shift+. to output a  >
     op.combination_key KEY_LEFTSHIFT, KEY_DOT
 
+    # press space key
     op.press_key KEY_SPACE
     op.release_key KEY_SPACE
   end
 end
 
-terminal_app_class = /(terminal|Xterm|Terminator)$/
+terminal_app_class = /(terminal|XTerm|Terminator)$/
 
 # special config must be defined before general.
 # e.g. nano is run in terminal, must be defined before terminal.
@@ -69,20 +76,19 @@ terminal_app_class = /(terminal|Xterm|Terminator)$/
 
 # For nano, app_class is Terminal, name is nano.
 window(@default_bind_resolver, :title => /nano/, :app_class => terminal_app_class) do
-  bind_key [KEY_LEFTALT, KEY_P], [KEY_LEFTALT, KEY_MINUS]
-  bind_key [KEY_LEFTALT, KEY_N], [KEY_LEFTALT, KEY_EQUAL]
-
-  bind_key [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_COMMA], [KEY_LEFTALT, KEY_BACKSLASH]
-  bind_key [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_DOT], [KEY_LEFTALT, KEY_SLASH]
+  # copy
+  bind_key [KEY_LEFTALT, KEY_W], [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_6]
+  # paste override global default paste.
+  bind_key [KEY_LEFTCTRL, KEY_Y], [KEY_LEFTCTRL, KEY_U]
 
   terminal_global
 end
 
 # For Terminal, must defined after nano
 window(@default_bind_resolver, :app_class => terminal_app_class) do
+  # copy
   bind_key [KEY_LEFTALT, KEY_W], [KEY_LEFTSHIFT, KEY_LEFTCTRL, KEY_C]
-  bind_key [KEY_LEFTCTRL, KEY_Y], [KEY_LEFTSHIFT, KEY_LEFTCTRL, KEY_V]
-  bind_key [KEY_LEFTCTRL, KEY_W], [KEY_LEFTSHIFT, KEY_LEFTCTRL, KEY_X]
+  # paste use global default paste.
 
   terminal_global
 end
