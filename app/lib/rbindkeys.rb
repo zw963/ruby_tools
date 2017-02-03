e0755dd181d3914d270156ea95db5013267b03fe
Billy.Zheng <zw963@163.com>
2017-02-03 17:43:17 +0800

    remove rubygems dependency.
    Former-commit-id: c2ea1bac9737a93a8458fb846727ebc6a5886efa

--- app/lib/rbindkeys.rb
+++ app/lib/rbindkeys.rb
@@ -1,6 +1,5 @@
 # -*- coding:utf-8; mode:ruby; -*-
 
-require 'rubygems'
 require 'rbindkeys/version'
 require 'rbindkeys/log_utils'
 
--- app/lib/ruinput/uinput_user_dev.rb
+++ app/lib/ruinput/uinput_user_dev.rb
@@ -1,6 +1,5 @@
 # -*- coding:utf-8; mode:ruby; -*-
 
-require 'rubygems'
 require 'revdev/each_values_equal'
 
 module Ruinput
