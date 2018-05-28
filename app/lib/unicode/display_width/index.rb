4addb8777b3a733f461f11ffe4cf1c928c9836de
Billy.Zheng <zw963@163.com>
2018-05-28 22:30:18 +0800

    update display_width gem
    Former-commit-id: 5a06277712ce8e24caaf9b7e717fe3a2a559e846

--- app/lib/unicode/display_width/index.rb
+++ app/lib/unicode/display_width/index.rb
@@ -1,7 +1,12 @@
+require 'zlib'
 require_relative 'constants'
 
 module Unicode
   module DisplayWidth
-    INDEX = Marshal.load(File.binread(INDEX_FILENAME))
+    File.open(INDEX_FILENAME, "rb") do |file|
+      serialized_data = Zlib::GzipReader.new(file).read
+      serialized_data.force_encoding Encoding::BINARY
+      INDEX = Marshal.load(serialized_data)
+    end
   end
 end
