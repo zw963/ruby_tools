9272d6a23b2e8bbd7043f3efc62d0fb5729176a2
Billy.Zheng <zw963@163.com>
2018-12-26 15:05:06 +0800

    Add Rubygems::Version
    Former-commit-id: 785537365622f62ddc7868c0632247fad4afc312

--- ruby/lib/ruby/2.5.0/rubygems.rb
+++ ruby/lib/ruby/2.5.0/rubygems.rb
@@ -1,9 +1,15 @@
-require 'thread'
-
 module Gem
   class LoadError < StandardError
+  end
+
+  class Deprecate
+    def self.skip
+      true
+    end
   end
 end
 
 def gem(*args)
 end
+
+require 'rubygems/version'
