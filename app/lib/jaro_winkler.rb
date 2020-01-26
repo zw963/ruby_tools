02bc7e24cc4e6ded3524892d914b4df4fc20b6a8
Billy.Zheng <vil963@gmail.com>
2020-01-26 13:39:36 +0800

    Update jaro_winkler

--- app/lib/jaro_winkler.rb
+++ app/lib/jaro_winkler.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 require 'jaro_winkler/version'
+require 'rbconfig'
 
 if RUBY_ENGINE == 'ruby'
   begin
-    require 'jaro_winkler/jaro_winkler_ext'
+    require "jaro_winkler/jaro_winkler_ext.#{RbConfig::CONFIG['ruby_version']}.#{RbConfig::CONFIG['DLEXT']}"
   rescue LoadError
     # Fall back to the pure implementation if the extension
     # can't be loaded for any reason (e.g. it was never built)
