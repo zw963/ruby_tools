019d131844a00ff27673bb8f4681c86479041079
Billy.Zheng <vil963@gmail.com>
2020-02-22 03:45:25 +0800

    Add coderay.

---
+++ app/lib/coderay_bash.rb
@@ -0,0 +1,10 @@
+require 'coderay'
+
+path = File.join File.expand_path(File.dirname(__FILE__))
+
+require File.join(path, "coderay/scanners/bash.rb")
+require File.join(path, "coderay/scanners/erb_bash.rb")
+
+# Register file types
+::CodeRay::FileType::TypeFromExt['sh'] = :bash
+::CodeRay::FileType::TypeFromExt['bash'] = :bash
