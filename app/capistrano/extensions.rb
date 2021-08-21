3be36570581b2d4d40347f040c62948e06861188
Billy.Zheng <vil963@gmail.com>
2021-08-21 18:47:48 +0800

    Update capistrano config_update methods

--- app/capistrano/extensions.rb
+++ app/capistrano/extensions.rb
@@ -8,3 +8,84 @@ def pid(cap_pid_file_sym)
   pid_file = fetch(cap_pid_file_sym)
   "`cat #{pid_file}`"
 end
+
+def config_update(ubuntu_config_path:, centos_config_path:, service_name:, restart_service_command:, check_config_command: nil, check_running_status_command: nil, args:)
+  if test '[[ $(cat /etc/*-release) =~ Ubuntu ]]'
+    system_config_dir = Pathname(ubuntu_config_path)
+  elsif test '[[ $(cat /etc/*-release) =~ CentOS ]]'
+    system_config_dir = Pathname(centos_config_path)
+  else
+    info "Current linux distribution not supported, skip `#{service_name}`!"
+    exit
+  end
+
+  if not args[:use_git].nil?
+    invoke 'git:clone'
+    invoke 'git:update'
+    invoke 'git:create_release'
+    invoke 'deploy:set_current_revision'
+    invoke 'deploy:symlink:linked_dirs'
+  end
+
+  deploy_to = capture("cd #{deploy_to()}; pwd")
+  project_config_dir = "#{deploy_to}/current/config/#{service_name}"
+  project_config_suffix = "_#{fetch(:stage)}"
+  # 这里使用 find, 是因为我们要获取服务上的文件列表，不是本地。
+  # 这里只是部署具有和当前 stage 后缀一样的配置文件，例如：stage 是 production,
+  # 这里只部署 nginx_prodution.conf, 而不部署 nginx_staging.conf
+  project_config_files = capture("find #{project_config_dir} -name *#{project_config_suffix}*").split("\n").map {|e| Pathname(e) }
+
+  should_reload_service = false
+
+  project_config_files.each do |project_config_file|
+    if project_config_file.extname.end_with? ".erb"
+      old_project_config_file = project_config_file
+      project_config_file = old_project_config_file.sub_ext('') # remove .erb suffix
+      ruby_script = <<-HEREDOC
+require "erb"
+require "yaml"
+erb = ERB.new(File.read("#{old_project_config_file}"))
+config = YAML.load_file("#{deploy_to}/current/system_config.yml").dig("#{service_name}", "#{fetch(:stage)}")
+File.write("#{project_config_file}", erb.result_with_hash(config.merge("config_base_name"=>"#{project_config_file.basename(".conf")}")))
+HEREDOC
+      info "generating #{project_config_file}"
+      execute "ruby -e '#{ruby_script}'"
+    end
+
+    system_config_name = project_config_file.relative_path_from(project_config_dir)
+    system_config_file = system_config_dir.join(system_config_name)
+    system_sub_conf_dir = system_config_file.dirname
+
+    # if system config exist, and two one no diff.
+    next if test "[ -e #{system_config_file} ] && diff #{system_config_file} #{project_config_file} -q"
+
+    execute "test -d #{system_sub_conf_dir} || sudo mkdir -pv #{system_sub_conf_dir}"
+
+    should_reload_service = true
+
+    # if system config exist, and new than project one, maybe someone changed it.
+    if test "[[ -e #{system_config_file} && #{system_config_file} -nt #{project_config_file} ]]"
+      # should backup it.
+      execute :sudo, "mv #{system_config_file} #{system_config_file}-#{Time.now.strftime('%Y-%m-%d_%H:%M:%S')}"
+    end
+
+    execute :sudo, "cp -a #{project_config_file} #{system_config_file}"
+  end
+
+  return info "Skip reboot #{service_name} because no config is changed." if should_reload_service == false
+
+  unless check_config_command.nil?
+    info 'Checking config'
+    execute :sudo, check_config_command
+  end
+
+  info 'Restarting service'
+  execute :sudo, restart_service_command
+
+  unless check_running_status_command.nil?
+    info 'Checking running status'
+    execute :sudo, check_running_status_command
+  end
+
+  info "#{service_name} is reloaded!"
+end
