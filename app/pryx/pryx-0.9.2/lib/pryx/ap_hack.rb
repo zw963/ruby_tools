518c38dc53fab1323ceb7d297b5fb1c4b8e4ffd3
Billy.Zheng <vil963@gmail.com>
2022-10-06 02:53:43 +0800

    update pryx to 0.9.2

--- app/pryx/pryx-0.9.2/lib/pryx/ap_hack.rb
+++ app/pryx/pryx-0.9.2/lib/pryx/ap_hack.rb
@@ -8,6 +8,7 @@ if defined? AwesomePrint
   if defined? Pry
     AwesomePrint.pry!
   else
+    # auto ap only works when running with `irbx`
     AwesomePrint.irb!
   end
 end
--- app/pryx/pryx-0.9.2/lib/pryx/version.rb
+++ app/pryx/pryx-0.9.2/lib/pryx/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Pryx
-  VERSION = [0, 8, 7]
+  VERSION = [0, 9, 2]
 
   class << VERSION
     include Comparable
